b7d2ea762f5453755fe397c14d57f828f519b746
Raul Escamilla <raul.escamilla@asesoriait.com>
2023-02-18 22:48:53 -0600

    Guia No 1

---
+++ Feb150223/Persona.java
@@ -0,0 +1,9 @@
+public class Persona {
+	public String dui;
+	public String nombre;
+	public String apellidos;
+	public String fechaNacimientol;
+	public String profesion;
+}
+
+
---
+++ Guia01/Producto.java
@@ -0,0 +1,53 @@
+public class Producto {
+	// Atributos de clase Producto
+	private String nombre;
+	private String descripcion;
+	private double precio;
+	private String codigo;
+	private String categoria;
+
+	// Getter y Setters para nombre
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	// Setters and Getters para descripcion
+	public String getDescripcion() { 
+		return descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	// Setters and Getters para precio
+	public double getPrecio() { 
+		return precio;
+	}
+	
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	// Setters and Getters para codigo
+	public String getCodigo() { 
+		return codigo;
+	}
+	
+	public String setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	// Setters and Getters para categoria
+	public String getCategoria() { 
+		return categoria;
+	}
+	
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+}
