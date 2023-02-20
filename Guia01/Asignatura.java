614f34fb7dfd86aa2d6fd898ff1ea9469620a93f
Raul Escamilla <raul.escamilla@asesoriait.com>
2023-02-19 20:59:47 -0600

    Libro y  Asignatura

---
+++ Guia01/Asignatura.java
@@ -0,0 +1,9 @@
+public class Asignatura
+{
+ 	public String nombre;
+	public String seccion;
+	public int estudiantess;
+	public int aula;
+	public int hora;
+}
+ 
---
+++ Guia01/Libro.java
@@ -0,0 +1,9 @@
+public class Libro
+{
+	public String codigo;
+	public String titulo;
+	public String autor;
+	public int paginas;
+	public double precio;
+}
+ 
